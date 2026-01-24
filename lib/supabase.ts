72e82ea9f72636e1bb6b8d5a3dbc8267eba36ae7
pranavmamatha <pranavranjithkannan@gmail.com>
2026-01-24 15:48:51 +0530

    creating client to connect with supabase and secure-storage adapter for phones

---
+++ lib/supabase.ts
@@ -0,0 +1,25 @@
+import { createClient } from "@supabase/supabase-js"
+import { deleteItemAsync, setItemAsync, getItemAsync } from "expo-secure-store"
+
+const ExpoSecureStore = {
+  getItem: (key: string) => {
+    return getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    return setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    return deleteItemAsync(key);
+  }
+}
+
+
+export const supabase = createClient(
+  process.env.EXPO_PUBIC_SUPABASE_URL,
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      storage: ExpoSecureStore,
+    }
+  }
+)
