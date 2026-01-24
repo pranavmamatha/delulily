8c82a3847db4ee54db3e24176c04846999624bb7
pranavmamatha <pranavranjithkannan@gmail.com>
2026-01-24 21:35:25 +0530

    zustand store created for storing sessions

---
+++ store/useAuthStore.ts
@@ -0,0 +1,38 @@
+import { Session } from "@supabase/supabase-js"
+import { create } from "zustand"
+
+type AuthType = {
+  session?: Session | null;
+  isLoading: boolean;
+  isLoggedIn: boolean;
+
+  setSession: (session: Session | null) => void;
+  setIsLoading: (loading: boolean) => void;
+  reset: () => void;
+}
+
+export const useAuthStore = create<AuthType>((set) => ({
+  session: undefined,
+  isLoading: true,
+  isLoggedIn: false,
+  setSession: (session) => {
+    set(() => ({
+      session,
+      isLoggedIn: !!session
+    }))
+  },
+  setIsLoading: (isLoading) => {
+    set(() => ({
+      isLoading
+    }))
+  },
+  reset: () => {
+    set(() => ({
+      session: null,
+      isloading: false,
+      isLoggedIn: false
+    }))
+  }
+}))
+
+
