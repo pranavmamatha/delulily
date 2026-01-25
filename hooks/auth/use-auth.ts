870a87a286a55c302e185d5de3ece2ce95d91810
pranavmamatha <pranavranjithkannan@gmail.com>
2026-01-25 10:26:24 +0530

    hook to provide the auth state

---
+++ hooks/auth/use-auth.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useAuthStore } from "@/store/useAuthStore";
+import { supabase } from "@/lib/supabase";
+
+export function useAuth() {
+  const { setSession, setIsLoading } = useAuthStore();
+  useEffect(() => {
+    const loadSession = async () => {
+      const { data: { session }, error } = await supabase.auth.getSession();
+      if (error) {
+        console.debug("Error while fetching session:", error)
+      }
+      setSession(session)
+      setIsLoading(false)
+    }
+    loadSession();
+
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+    return () => subscription.unsubscribe();
+  }
+    , [])
+}
