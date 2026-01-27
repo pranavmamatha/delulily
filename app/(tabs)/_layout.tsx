d2e48e6359fdabe884d9b7192693378cd75fcd48
pranavmamatha <pranavranjithkannan@gmail.com>
2026-01-27 16:42:28 +0530

    added native wind to the project

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Slot } from "expo-router"
+
+const _layout = () => {
+  return (
+    <Slot />
+  )
+}
+
+export default _layout
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+export default function Home() {
+  return (
+    <View className="flex flex-1 justify-center items-center">
+      <Text className="bg-pink-400">
+        You are Home!!!
+      </Text>
+    </View>
+  )
+}
